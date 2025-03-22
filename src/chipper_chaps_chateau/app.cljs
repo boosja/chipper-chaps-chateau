109ed9e5a710a8444f6066c0e0911387b795fb17
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-03-22 11:21:54 +0100

    Cleanup, organize and alias-ize

--- src/chipper_chaps_chateau/app.cljs
+++ src/chipper_chaps_chateau/app.cljs
@@ -1,6 +1,8 @@
 (ns chipper-chaps-chateau.app
-  (:require [chipper-chaps-chateau.victory :as victory]
-            [clojure.string :as str]
+  (:require [chipper-chaps-chateau.db :as db]
+            [chipper-chaps-chateau.el-prepare :as el-prepare]
+            [chipper-chaps-chateau.la-visual :as vis]
+            [chipper-chaps-chateau.victory :as victory]
             [datascript.core :as ds]
             [replicant.dom :as d]))
 
@@ -10,33 +12,19 @@
 ;; time barrier
 ;; know when game is won ✓
 ;; - also when tied
-;; show color on hover
+;; show color on hover?
+;; clean up and alias ✓
 
-(defn chips []
-  (for [i (range 3)
-        j (range 3)
-        k (range 3)]
-    (let [x (inc j)
-          y (inc i)
-          z (inc k)]
-      {:x x :y y :z z :pawn/idx (+ (* 100 y) (* 10 x) (* 1 z))})))
-
-(def txes (concat [{:db/id "player one"
+(def txes (concat [{:db/ident :current-color
+                    :current-color :blue}
+                   {:db/id "player one"
                     :player/color :blue
                     :chip.lg/count 3
                     :chip.md/count 3
-                    :chip.sm/count 3}
-                   {:db/ident :current-color
-                    :current-color :blue}]
-                  (chips)))
+                    :chip.sm/count 3}]
+                  (db/chips-txes)))
 
-(def schema {:player/color {:db/cardinality :db.cardinality/one
-                            :db/unique :db.unique/identity}
-
-             :player/pawns {:db/valueType :db.type/ref
-                            :db/cardinality :db.cardinality/many}
-
-             :pawn/idx {:db/cardinality :db.cardinality/one
+(def schema {:chip/idx {:db/cardinality :db.cardinality/one
                         :db/unique :db.unique/identity}})
 
 (defonce conn
@@ -44,121 +32,38 @@
     (ds/transact! conn txes)
     conn))
 
-(comment
-
-  (def db (ds/db conn))
-
-  (->> (ds/q '[:find [?e ...]
-               :where [?e :player/pawns]]
-             db)
-       (mapv #(ds/entity db %))
-       (mapv #(into {} %))
-       (mapv (fn [pl] (update pl :player/pawns (fn [ps] (mapv #(into {} %) ps))))))
-
-  (->> (ds/entity db [:pawn/idx 3])
-      (into {}))
-
-  (->> (ds/q '[:find [?e ...]
-               :where
-               [?e :x ?x]]
-             db)
-       (map #(ds/entity db %))
-       (sort-by :idx)
-       (map #(into {} %)))
-
-  (into {} (ds/entity db :current-color))
-  (:current-color (ds/entity db :current-color))
-
-  )
-
-(defn get-current-color [db]
-  (:current-color (ds/entity db :current-color)))
-
 (defn actions-handler [metadata actions]
   (let [_ (:replicant/js-event metadata)]
     (doseq [[action & args] actions]
       (apply prn 'Execute action args)
       (case action
-        :action/transact (ds/transact conn args)))))
-
-(defn get-chips [db]
-  (->> (ds/q '[:find [?e ...]
-               :where
-               [?e :x ?x]]
-             db)
-       (map #(->> % (ds/entity db) (into {})))
-       (sort-by :pawn/idx)))
-
-(defn ->cells [chips]
-  (->> chips
-       (partition 3)
-       (mapv (comp vec flatten))))
-
-(def next-color
-  {:blue :red
-   :red :green
-   :green :yellow
-   :yellow :blue})
+        :action/transact (apply ds/transact conn args)))))
 
 (comment
   (def db (ds/db conn))
 
-  (def cs (get-chips db))
+  (def cs (db/get-chips db))
   cs
 
-  (->> cs
-       (filter :colored)
-       (group-by :colored)
-       (victory/vals->sets)
-       (some victory/has-three-in-a-row?))
+  (->> cs (partition 3) (mapv (fn [[l m s]] [{:size :lg :color (:chip/color l :white)}
+                                             {:size :md :color (:chip/color m :white)}
+                                             {:size :sm :color (:chip/color s :white)}])))
 
   (victory/did-someone-win? cs)
 
   )
 
-(defn click-action [chip current-color]
-  [[:action/transact {:pawn/idx (:pawn/idx chip)
-                      :colored current-color}]
-   [:action/transact {:db/ident :current-color
-                      :current-color (next-color current-color)}]])
-
-(defn winner-section [winner]
-  [:div.box.winner
-   [:span.tada "🎉"]
-   [:div.current {:class (and winner (name winner))}
-    (str (str/capitalize (name winner)) " is the winner")]
-   [:span.tada "🎉"]])
-
 (defn app [db]
-  (let [current-color (get-current-color db)
-        chips (get-chips db)
-        cells (->cells chips)
-        winner (victory/did-someone-win? chips)]
+  (let [state (el-prepare/prepare db)]
     [:main
      [:h1 "Chipper Chap's Chateau"]
-     (if winner
-       (winner-section winner)
-       [:div.box
-        [:div.current {:class (name current-color)}
-         "Current player"]])
-     [:section.wrapper
-      [:div.board
-       (for [[large medium small] cells]
-         [:div.cell
-          [:div.chip.large
-           {:class (:colored large :white)
-            :on {:click (when (and (not winner) (nil? (:colored large)))
-                          (click-action large current-color))}}]
-
-          [:div.chip.medium
-           {:class (:colored medium :white)
-            :on {:click (when (and (not winner) (nil? (:colored medium)))
-                          (click-action medium current-color))}}]
-
-          [:div.chip.small
-           {:class (:colored small :white)
-            :on {:click (when (and (not winner) (nil? (:colored small)))
-                          (click-action small current-color))}}]])]]]))
+     (vis/player-box (:current-color state)
+                     (:game-won? state))
+     [:div.wrapper
+      [::vis/board.board
+       {::vis/data (:cells state)}
+       [::vis/cell.cell
+        [::vis/chip.chip]]]]]))
 
 (defn ^:dev/after-load start []
   (js/console.log "[START]")
---
+++ src/chipper_chaps_chateau/db.cljc
@@ -0,0 +1,29 @@
+(ns chipper-chaps-chateau.db
+  (:require [datascript.core :as ds]))
+
+(defn get-global [db k]
+  (get (ds/entity db k) k :404))
+
+(defn ->global-tx [k v]
+  {:db/ident k k v})
+
+(defn chips-txes []
+  (for [i (range 3)
+        j (range 3)
+        k (range 3)]
+    (let [x (inc j)
+          y (inc i)
+          z (inc k)]
+      {:x x
+       :y y
+       :z z
+       :chip/size (get {1 :lg 2 :md 3 :sm} z)
+       :chip/idx (+ (* 100 y) (* 10 x) (* 1 z))})))
+
+(defn get-chips [db]
+  (->> (ds/q '[:find [?e ...]
+               :where
+               [?e :x ?x]]
+             db)
+       (map #(->> % (ds/entity db) (into {})))
+       (sort-by :chip/idx)))
