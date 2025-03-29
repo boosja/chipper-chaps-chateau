77893bce754180a5969ad4f5a897485b6fc752d0
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-03-29 14:17:58 +0100

    Log general views to / too

--- src/chipper_chaps_chateau/server.clj
+++ src/chipper_chaps_chateau/server.clj
@@ -1,23 +1,31 @@
 (ns chipper-chaps-chateau.server
   (:require [clojure.java.io :as io]
-            [compojure.core :refer [defroutes HEAD]]
+            [compojure.core :refer [defroutes HEAD wrap-routes]]
             [compojure.route :as route]
             [org.httpkit.server :as server]))
 
 (def storage (System/getenv "GARDEN_STORAGE"))
 
-(defn log-404 [edn]
-  (let [file (str storage "/bads/unwanted-attention.log")]
+(defn ->smap [m]
+  (-> {:date (str (java.time.Instant/now))}
+      (merge m)
+      (str "\n")))
+
+(defn log->file [path m]
+  (let [file (str storage path)]
     (io/make-parents file)
-    (spit file edn :append true)))
+    (spit file (->smap m) :append true)))
+
+(defn log-stats [handler]
+  (fn [request]
+    (when (= (:uri request) "/")
+      (log->file "/stats.log" {:uri (:uri request)}))
+    (handler request)))
 
 (defn not-found-inspector [req]
-  (let [[uri method] ((juxt :uri :request-method) req)
-        edn (str {:date (str (java.time.Instant/now))
-                  :method method
-                  :uri uri}
-                 "\n")]
-    (log-404 edn)
+  (let [[uri method] ((juxt :uri :request-method) req)]
+    (log->file "/bads/unwanted-attention.log"
+               {:method method :uri uri})
     {:body "<html><body><h1>404 Not Found</h1></body></html>"}))
 
 (defroutes app-routes
@@ -27,7 +35,8 @@
   (route/not-found not-found-inspector))
 
 (def wrapped-app
-  app-routes)
+  (-> app-routes
+      (wrap-routes log-stats)))
 
 (defn start! [opts]
   (let [server (server/run-server #'wrapped-app
