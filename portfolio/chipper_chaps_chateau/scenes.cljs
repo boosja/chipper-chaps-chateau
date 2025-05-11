bd97d955a1c08bbd7f00f656284673586075027b
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-05-11 19:34:08 +0200

    Add colorblind-friendly colors

--- portfolio/chipper_chaps_chateau/scenes.cljs
+++ portfolio/chipper_chaps_chateau/scenes.cljs
@@ -1,14 +1,17 @@
 (ns chipper-chaps-chateau.scenes
   (:require [chipper-chaps-chateau.bar-scenes]
             [chipper-chaps-chateau.bartial-scenes]
+            [chipper-chaps-chateau.el-chateau-scenes]
             [portfolio.ui :as ui]
             [replicant.dom :as d]))
 
 :chipper-chaps-chateau.bar-scenes/keep
 :chipper-chaps-chateau.bartial-scenes/keep
+:chipper-chaps-chateau.el-chateau-scenes/keep
 
 (ui/start!
- {:config {:css-paths ["/css/style.css"]}})
+ {:config {:css-paths ["/css/style.css"
+                       "/css/portfolio.css"]}})
 
 (defn init [])
 
