cca9cf3e6269864e789eda1e0d83aebd45ce6735
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-05-12 21:02:20 +0200

    Configure cider to repl both chipper and portolio 💪

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((nil
-  (cider-clojure-cli-aliases . "-A:dev")))
+((nil . ((cider-clojure-cli-aliases . "-A:dev")
+         (cider-default-cljs-repl . shadow)
+         (cider-shadow-default-options . "chipper-chaps-chateau")
+         (cider-shadow-watched-builds . ("chipper-chaps-chateau" "portfolio")))))
