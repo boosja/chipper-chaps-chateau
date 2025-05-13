e798e159e6befa20ec1a8c22ee330c776f5bea37
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-05-13 15:22:35 +0200

    Add the awesome Dataspex data inspector

---
+++ dev/chipper_chaps_chateau/dev.cljs
@@ -0,0 +1,9 @@
+(ns chipper-chaps-chateau.dev
+  (:require [chipper-chaps-chateau.app :as app]
+            [dataspex.core :as dataspex]))
+
+(comment
+
+  (dataspex/inspect "Chipper DB" app/conn)
+
+  )
