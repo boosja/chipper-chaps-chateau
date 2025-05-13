db780d89a4a9490e5f18233742db9315ac23728b
boosja <mathias.iversen487@gmail.com>
2025-05-13 15:04:28 +0200

    Add better example board and scoring-logic

--- notebooks/scoring.clj
+++ notebooks/scoring.clj
@@ -25,6 +25,14 @@
 ^{::clerk/visibility {:code :hide}}
 (clerk/html [:h1 "Scoring the board for choosing the next move"])
 
+^{::clerk/visibility {:code :hide}}
+(clerk/html [:div {:style {:color "black"
+                           :background "yellow"
+                           :padding "1rem"
+                           :text-align "center"
+                           :border "1px solid black"}}
+             "WIP - WORK IN PROGRESS - WIP"])
+
 ;; First we have our winning scenarios. Each set in the list is a winning line
 ;; consisting of three points (chips) in a three-dimensional grid.
 ^{::clerk/auto-expand-results? false}
@@ -42,25 +50,20 @@
                                     (rest %)
                                     %)))])
 
-;; To ease our way forward we can create a point-to-color map. This way it'll be
-;; a breeze to find which point has what color.
-^{::clerk/auto-expand-results? true}
-(victory/->point-color-mapper example-board)
-
-;; Then we want to merge the colored points with the winning lines defined
-;; above to get a better idea where the different colors lie regards to
-;; winning.
+;; To ease our way forward we can merge the board points into the winning lines
+;; defined above to get a better idea where the different colors lie in regard
+;; to winning.
 (def colored-wins (victory/merge-wins-with-colors victory/wins example-board))
 
-;; We group the now colored winning lines by each point contained in each set of
-;; winning lines.
+;; Let's group them by point:
 ^{::clerk/visibility {:result :hide}}
 (def wins-grouped-by-point (victory/group-by-point colored-wins))
 ^{::clerk/visibility {:code :hide}}
 (->sorted-map (victory/group-by-point colored-wins))
 
-;; We are now able to loop through the points and for every winning line
-;; containing the point calculate the frequency of each color per winning line.
+;; We are now able to loop through the points, and for every winning line
+;; containing the point, calculate the frequency of each color.
+
 ;; This gives us:
 ^{::clerk/visibility {:result :hide}}
 (def stats (victory/stats wins-grouped-by-point))
@@ -71,14 +74,16 @@
 
 ;; Lets take a look at the first uncolored point (we are not interested in
 ;; points that are already colored in).
-(first (filter #(-> % first :chip/color nil?) stats))
+(def point (first (filter #(-> % first :chip/color nil?) stats)))
 
 ;; The result shows us the frequency of the colors in the winning lines the
-;; point is a part of. Of the seven winning lines this point is a part of, six
-;; does not have a single point that is `:blue`, but one has two!
+;; point is a part of.
 
-;; There is one winning line with a single `:green` point, two with one `:red`
-;; and no winning line has a yellow.
+;; Of the four winning lines this point is a part of, three
+;; does not have a single point that is `:blue`, but one does!
+
+;; There is one winning line with a single red point, one with a single
+;; yellow point, no winning line has a green.
 
 ;; Scoring the board points are now possible, but before we do we need to put
 ;; values to the different frequencies of colors for each point. For instance,
@@ -89,26 +94,26 @@
 
 ;; Winning lines containing current color:
 ;; 0 times are valued as 1
-;; 1 times are valued as 2
-;; 2 times are valued as 10
+;; 1 times are valued as 10
+;; 2 times are valued as 100
 ^{::clerk/visibility {:code :hide}}
 (clerk/html [:div.flex.justify-center
              (clerk/table {"frequency" [0 1 2]
-                           "valued as" [1 2 10]})])
+                           "valued as" [1 10 100]})])
 
-;; So to calculate the score of a point with the winning line frequency map `{0 6,
-;; 2 1}`, we can do:
+;; So to calculate the score of a point for color blue
+(def blue (:blue (second point)))
+;; we can do:
 
-;; ```
-;; (* 1 6) => 6
-;; (* 10 1) => 10
-;; = 16
-;; ```
+(+ (* 1 3)
+   (* 10 1))
 
-(reduce (fn [sum [freq n]]
-          (+ sum (* (get {0 1, 1 2, 2 10} freq) n)))
-        0 {0 6, 2 1})
+(reduce (fn [s [freq n]]
+          (+ s (* (get {0 1, 1 10, 2 100} freq) n)))
+        0
+        blue)
 
-;; (+ (* 6 1)   ;; 0 are valued as 1
-;;    (* 1 10)) ;; 2 are valued as 10
-;; => 10
+;; For all points and colors:
+(->> (filter #(nil? (-> % first :chip/color)) stats)
+     victory/calc-scores
+     (sort-by second victory/compare-point-scores))
