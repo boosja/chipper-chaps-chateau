b4073552f96788bd76d955b475323d4c1cbfb94e
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-06-03 17:57:58 +0200

    Hardcode the wins

--- dev/chipper_chaps_chateau/dev.clj
+++ dev/chipper_chaps_chateau/dev.clj
@@ -1,5 +1,6 @@
 (ns chipper-chaps-chateau.dev
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [chipper-chaps-chateau.wins :as wins]
+            [nextjournal.clerk :as clerk]))
 
 (comment
 
@@ -11,5 +12,34 @@
 
   (clerk/build! {:paths ["notebooks/*"]
                  :out-path "public/clerk"})
+  )
+
+(comment
+
+  (def d3 (wins/find-collinear-triplets (for [y (range 1 4)
+                                              x (range 1 4)
+                                              z (range 1 4)]
+                                          [x y z])))
+
+  (def d4 (wins/find-collinear-triplets (for [w (range 1 4)
+                                              y (range 1 4)
+                                              x (range 1 4)
+                                              z (range 1 4)]
+                                          [x y z w])))
+
+  (def d5 (wins/find-collinear-triplets (for [v (range 1 4)
+                                              w (range 1 4)
+                                              y (range 1 4)
+                                              x (range 1 4)
+                                              z (range 1 4)]
+                                          [x y z w v])))
+
+  (defn wins->edn [path wins]
+    (->> (str "[" (clojure.string/join "\n " wins) "]")
+         (spit path)))
+
+  (wins->edn "public/wins-3d.edn" wins/d3)
+  (wins->edn "public/wins-4d.edn" wins/d4)
+  (wins->edn "public/wins-5d.edn" wins/d5)
 
   )
