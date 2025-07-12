7781982e7148ccf97affa1db95a9b94fbf41ad55
boosja <mathias.iversen487@gmail.com>
2025-07-12 15:00:35 +0200

    Dev-centered logic should be in the dev-ns

--- dev/chipper_chaps_chateau/dev.cljs
+++ dev/chipper_chaps_chateau/dev.cljs
@@ -1,6 +1,58 @@
 (ns chipper-chaps-chateau.dev
   (:require [chipper-chaps-chateau.app :as app]
+            [chipper-chaps-chateau.chips :as chips]
+            [chipper-chaps-chateau.db :as db]
+            [chipper-chaps-chateau.id :as id]
+            [chipper-chaps-chateau.victory :as victory]
+            [datascript.core :as ds]
             [dataspex.core :as dataspex]))
+
+(defn override-board! [chips]
+  (ds/transact app/conn [{:db/id "new-game"
+                          :game/id (id/gen!)
+                          :game/current-color :blue
+                          :game/chips (id/-ilize! :chip/id chips)}
+                         {:db/ident :app/state
+                          :app/current-game "new-game"}]))
+
+(comment
+  (def db (ds/db app/conn))
+
+  (def current-game (db/current-game db))
+  (->> (:game/chips current-game)
+       (map #(into {} %))
+       (sort-by (comp vec reverse :point))
+       (partition-all 27))
+
+  (def cs (db/get-chips db))
+  cs
+
+  (override-board! (chips/colored-chateaus))
+  (override-board! (chips/colored-chateaus-switch :blue))
+  (override-board! (chips/colored-chateaus-switch :red))
+  (override-board! (chips/colored-chateaus-switch :green))
+  (override-board! (chips/colored-chateaus-switch :yellow))
+  (override-board! (chips/colored-chateaus-switch-cycle))
+  (override-board! (victory/heat-mapped-chips))
+  (override-board! (victory/heat-mapped-chips-4d))
+  (override-board! (victory/heat-mapped-chips-5d))
+
+  ;; Reset game
+  (override-board! (chips/create-chips :dim/three))
+
+  ;; Navigate
+  (do (app/dispatch nil [[:action/navigate :route/d1]])
+      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/one))))
+  (do (app/dispatch nil [[:action/navigate :route/d2]])
+      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/two))))
+  (do (app/dispatch nil [[:action/navigate :route/d3]])
+      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/three))))
+  (do (app/dispatch nil [[:action/navigate :route/d4]])
+      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/four))))
+  (do (app/dispatch nil [[:action/navigate :route/d5]])
+      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/five))))
+
+  )
 
 (comment
 
--- src/chipper_chaps_chateau/app.cljs
+++ src/chipper_chaps_chateau/app.cljs
@@ -11,7 +11,6 @@
             [chipper-chaps-chateau.id :as id]
             [chipper-chaps-chateau.rules-page :as rules-page]
             [chipper-chaps-chateau.settings :as settings]
-            [chipper-chaps-chateau.victory :as victory]
             [clojure.walk :as walk]
             [datascript.core :as ds]
             [replicant.dom :as d]))
@@ -66,53 +65,6 @@
   (let [conn (ds/create-conn schema)]
     (ds/transact! conn txes)
     conn))
-
-(defn override-board! [chips]
-    (ds/transact conn [{:db/id "new-game"
-                        :game/id (id/gen!)
-                        :game/current-color :blue
-                        :game/chips (id/-ilize! :chip/id chips)}
-                       {:db/ident :app/state
-                        :app/current-game "new-game"}]))
-
-(comment
-  (def db (ds/db conn))
-
-  (def current-game (db/current-game db))
-  (->> (:game/chips current-game)
-       (map #(into {} %))
-       (sort-by (comp vec reverse :point))
-       (partition-all 27))
-
-  (def cs (db/get-chips db))
-  cs
-
-  (override-board! (chips/colored-chateaus))
-  (override-board! (chips/colored-chateaus-switch :blue))
-  (override-board! (chips/colored-chateaus-switch :red))
-  (override-board! (chips/colored-chateaus-switch :green))
-  (override-board! (chips/colored-chateaus-switch :yellow))
-  (override-board! (chips/colored-chateaus-switch-cycle))
-  (override-board! (victory/heat-mapped-chips))
-  (override-board! (victory/heat-mapped-chips-4d))
-  (override-board! (victory/heat-mapped-chips-5d))
-
-  ;; Reset game
-  (override-board! (chips/create-chips :dim/three))
-
-  ;; Navigate
-  (do (dispatch nil [[:action/navigate :route/d1]])
-      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/one))))
-  (do (dispatch nil [[:action/navigate :route/d2]])
-      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/two))))
-  (do (dispatch nil [[:action/navigate :route/d3]])
-      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/three))))
-  (do (dispatch nil [[:action/navigate :route/d4]])
-      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/four))))
-  (do (dispatch nil [[:action/navigate :route/d5]])
-      (override-board! (id/-ilize! :chip/id (chips/create-chips :dim/five))))
-
-  )
 
 (defn perform-actions [db actions]
   (mapcat (fn [action]
